0ba2acf042a0457ecf68f03382dd8ee037a3f877
Hudson Ross <hudson@sejongconsortium.com>
2025-10-05 22:17:38 -0700

    Fixed with dollar operator

--- 3 - dollar notation/main.hs
+++ 3 - dollar notation/main.hs
@@ -1,14 +1,14 @@
-addNumber x = x + x
+addNumbers x = x + x
 
-main = print addNumbers x --doesn't work. 'print' is expecting one parameter, not two.
+main = print $ addNumbers 1 
 
-{-
-Option (1) We can enclose addNumbers x in parens i.e. 'main = print (addNumbers x)
+{-works. Now addNumbers will eval with its argument, and gain a return value.
 
-Option (2) Use the $ operator, the " " itself is an operator that binds the argument to its right to the function to its left.
+-addNumbers will take 1, then calculate 1 + 1, which is 2.
 
-However, Haskell evaluates from left to right, so without modifications, Haskell will try to do 'print addNumbers' without 'addNumbers' 
-finishing its calculation based on its input, x.
+-2 will then be returned to the function 'print'.
 
 -}
+
+
 
