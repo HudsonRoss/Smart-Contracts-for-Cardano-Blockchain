fd012b524ab7dc85b9fea299500f35dd26196044
Hudson Ross <hudson@sejongconsortium.com>
2025-10-05 21:13:33 -0700

    Example of passing multiple vars to the function, but printing just one.

--- 1st task/main.hs
+++ 1st task/main.hs
@@ -1,5 +1,11 @@
-setDate date = putStrLn date
+setDate day month year = putStrLn day
 
 dateArgument = "July 1st, 2009"
 
-main = setDate dateArgument
+day = "1st"
+
+month = "July"
+
+year = "2009"
+
+main = setDate day month year
